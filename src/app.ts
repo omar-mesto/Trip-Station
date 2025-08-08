190dade1810d99803716103b703129c7fb8e6aa8
MesudAhmed <mesudahmed403@gmail.com>
2025-08-08 04:56:02 +0300

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import connectDB from './config/db';
+import userRoutes from './routes/user.routes';
+import adminRoutes from './routes/admin.routes';
+import companyRoutes from './routes/company.routes';
+import { errorHandler } from './middlewares/error.middleware';
+import { notFoundHandler } from './middlewares/notFound.middleware';
+
+dotenv.config();
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api/admin', adminRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/companies', companyRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running ${PORT}`));
+});
