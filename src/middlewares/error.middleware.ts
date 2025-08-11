0e51d60358465204b707c465ffb20eef82cf3b00
omar-mesto <omisto014@gmail.com>
2025-08-12 02:44:52 +0300

    Fix Error in Error Middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
+import { t } from "../config/i18n";
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  const lang = (req.query.lang as string) || "en";
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
     success: false,
-    message: err.message || req.t('unexpected_error'),
+    message: err.message || t('unexpected_error', lang as any),
     error: process.env.NODE_ENV === 'development' ? err.stack : {},
   });
 };
