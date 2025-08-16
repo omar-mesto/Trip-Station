4137f79dd8c1ca2b8719624e33aa4a3b09004da1
omar-mesto <omisto014@gmail.com>
2025-08-16 04:50:03 +0300

    Create Get Users Api for Admin

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -2,9 +2,22 @@ import { Request, Response } from 'express';
 import { successResponse } from '../utils/response';
 import { asyncHandler } from '../middlewares/asyncHandler';
 import { getDashboardStatsService } from '../services/admin.service';
+import { listUsersService, toggleBlockUserService } from '../services/admin.service';
 
 export const getDashboardStats = asyncHandler(async (req: Request, res: Response) => {
   const lang = req.lang!;
   const stats = await getDashboardStatsService(lang);
   return successResponse(res, stats);
+});
+
+export const getUsers = asyncHandler(async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as any) || 1;
+  const limit = parseInt(req.query.limit as any) || 10;
+  const data = await listUsersService(page, limit);
+  return successResponse(res, data);
+});
+
+export const toggleBlockUser = asyncHandler(async (req: Request, res: Response) => {
+  const user = await toggleBlockUserService(req.params.id);
+  return successResponse(res, user);
 });
