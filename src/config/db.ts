a3d569d10ec44aa1b58034a33562e1e762d1ca8c
omar-mesto <omisto014@gmail.com>
2025-08-17 06:51:22 +0300

    Fixing Integrity Error

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB Connection Failed');
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   }
 };
